1e8656dbbcf12b9ebaf1246ea53cb9db2cba37ab
wangko27 <wangkun@nuls.io>
2019-06-15 17:57:24 +0800

    Init

--- src/main.js
+++ src/main.js
@@ -1,34 +1,15 @@
-import Vue from 'vue';
-import iView from 'iview';
-import VueRouter from 'vue-router';
-import Routers from './router';
-import Util from './libs/util';
-import App from './app.vue';
-import 'iview/dist/styles/iview.css';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-Vue.use(VueRouter);
-Vue.use(iView);
+Vue.config.productionTip = false
 
-// 路由配置
-const RouterConfig = {
-    mode: 'history',
-    routes: Routers
-};
-const router = new VueRouter(RouterConfig);
-
-router.beforeEach((to, from, next) => {
-    iView.LoadingBar.start();
-    Util.title(to.meta.title);
-    next();
-});
-
-router.afterEach((to, from, next) => {
-    iView.LoadingBar.finish();
-    window.scrollTo(0, 0);
-});
-
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router: router,
-    render: h => h(App)
-});
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
